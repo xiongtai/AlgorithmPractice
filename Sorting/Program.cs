d0d099d86deb425738f5fcb8740499d06823bcd4
xiongtai <tai.xiong@pactera.com>
2018-08-10 17:35:05 +0800

    Add project files.

---
+++ Sorting/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Sorting
+{
+    class Program
+    {
+        public delegate void SortingMethod(int[] array);
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 3, 2, 4 };            
+            Info(Sorting.BubbleSorting, a);
+
+            Console.ReadLine();
+        }
+
+        static void PrintArray<T>(T[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+        static void Info(SortingMethod sort, int[] array)
+        {
+            Stopwatch sp = new Stopwatch();
+            sp.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                var test = array.Clone();
+                sort.Invoke(array);
+            }
+            sp.Stop();
+            Console.WriteLine(sp.ElapsedMilliseconds);
+            PrintArray(array);
+        }
+
+
+    }
+}
---
+++ Sorting/Sorting.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting
+{
+    static class Sorting
+    {
+        public static void BubbleSorting(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        array.Swap(j, j + 1);
+                    }
+                }
+            }
+        }
+        static void Swap(ref int one, ref int two)
+        {
+            int temp = one;
+            one = two;
+            two = temp;
+        }
+
+        static void Swap(this int[] array, int one, int two)
+        {
+            int temp = array[one];
+            array[one] = array[two];
+            array[two] = temp;
+        }
+
+        static void MergeArray(int[] array)
+        {
+            
+        }
+
+        static void SplitArray(int[] array1,int[] array2)
+        {
+
+        }
+
+        static void SplitMergeArray(int[] array)
+        {
+
+        }
+    }
+}
